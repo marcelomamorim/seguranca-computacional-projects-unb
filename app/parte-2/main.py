ae5ebff67637420b970d5ef9ff3f8805ba7f8fb5
Mauro F Almeida <maurofda@gmail.comm>
2023-05-06 18:26:00 -0300

    ultils.py -> remove_accents.py main.py desafio1.txt desafio2.txt

--- app/parte-2/main.py
+++ app/parte-2/main.py
@@ -17,6 +17,41 @@ kakv tf thv syip.  khvre zs eotyieg slrgrijieg ie tyis.  zf khep blt
 keen it, rldosk acl mvn zn tyezr dvgiee, jode tzmv or ftyer, thvrijh
 merp nvarcy khe jade fvecinxs kowrrus tye fcern nity mv.'''
 
+
+desafio_txt2 = '''tpsja kexis ttgztpb wq ssmil tfdxf vsetw ytafrttw btzf 
+pcbroxdzo zn tqac wix, bwfd s, je ahvup sd pcbqqxff lfzed d avu ytwoxavneh 
+sg p aznst qaghv. sfiseic f udh zgaurr dxnm rcdentv btzf nllgubsetz, wymh 
+qfndbhqgotopl qq asmactq m prftlk huusieymi ythfdz: t tdxavict i cjs vu yts 
+edi grzivupavnex yy pikoc wirjbko, xtw gb rvffgxa pikoc, iedp elex t gmbdr fzb 
+sgiff bpkga; p gvgfghm t ele z xwogwko qbgmgwr adlmy bozs rtpmchv e xtme ccmo. 
+xhmetg, hup meyqsd czgxaj o jul fsdis, eaz t tah bf iymvaxhf, mll ra roso: 
+objqgsecl kepxqrl pgxdt sjtp emhgc v o axrfphvunh. huic zseh, ijewiet tw pjoj 
+hzkee so kacwi pt ida dxbfp-tvict ha bsj dp tkahhf dp 1869, ge yxbya mxpm rvrclke 
+pt qrtfffu. iwehl nre hsjspgxm t elaeks mccj, rtcse t diodiiddg, vrl lsxiszrz, 
+isehiza nxvop rv tcxdqchfs nhrfdg v ffb eodagayaepd of cpfmftfzo ahv acnv axbkah. 
+cezp tquvcj! vpkhmss v qfx rmd vfugx gmghrs yxq mciecthw. mrfvsnx ugt qyogbe — 
+btbvictzm jar csnzucvr mtnhm, ifzsex i odbjtlgxq, iof czgwfpbke p mea ifzsex, ugt 
+zvvzn yy sohupeie uwvid we gahzml asdp o znexvopzrr plxm tbxeyasep wuett ra swjcfkwa 
+fiv pchjqgwl a mxmdp rv mtglm rcma: — “ghw, cjs f czglqrsjtpl, qqjg jeyasdtg, mod 
+isptwj dtsid rcdirh ugt o eaenvqoo gacxgq tgkac vlagoedz t tqgrr ickibpfrvpe hq ja uod 
+feuh pvlzl gmgottpkie fiv tpf lacfrdz t lgboeiothq. tgke lk wabpiiz, xwfpg xoetw pd qvu, 
+ljyqaoj nfoizh sjcfkee fiv czuvqb c rzfe gabc lm nkibt tlnpkia, iiuo tlwa t o uoc vvgp s 
+da bni xws iot t rmiiiekt ee bozs tgxuboj eymvmcvrs; enha xgjo p nq ejpcixx pajjfr lh 
+rahgf iwnwfgs wiytha.” qcd e qbix pazgz! gea, cof mp tvdtdvnoh hmh jznex ebdzzcpl ugt zye 
+oxmjtw. v fzb eehwd qfx gttulet t gxpijuwt hah avud wmmh; tfi llwub ele xx izrodiyaiu eoia z 
+nrpxgtogxvqs qfuymvk ss yaxeif, hsd ad âgwupg eex tw pjjzdll ha bcto akmzrwge, xtw bpijaoh i 
+fgcgerh gabc hupf wq gskict xmgrv dz xwbthrcfes. fpfue p tfagfvctws. hxfrmxx md jars yhzq di 
+uek iiehcrs, pgxdt scad mvqh gvnshvmh, aznst mdbo jambrm, rojaot gab c toekmy, p tzlst, — yy 
+awiiz ws hpzv, — e... exrtpa ganbizrwr! dljyu p dfunh pttg uicxm cjsd ect e ftftetke etbyoct. 
+gachvnexq-et rv sluid fiv edle mcceixt, eucrr qfx rmd drrpgxm, eouenxy ypwj dz jyq pg gacxrfpg. 
+v vpkhmss, gaoxgqj arid. gea swxo bni et qrrabwet, bro obka fiv sp wiumojsp ksxpf gewh gtpc, 
+toyoyxho. eex h qqj csieh idp qfidt exiodeymi pgodaebgm... ja jowmiugof qfx ijewia lhw etgjeyme 
+q firtch ezdg, eaz iedtqv qfx vqjbr ex lm fdrfs zl ixtavnehw pt ida ekestrza. p wepd ele dbq, a 
+fiv mpgse rcevtglm p sjsl tracwda pke meoieyme-xd. rv pp, t gmqstetke pp qrml, vsy dg flshw 
+qhhlptwse, p pfcl xrfgsrbpkxm, p hiidmi etbyoct qma dfdtt gdtf ea xbrtp sottggmd.
+
+'''
+
 #################################################################################################################
 #################################################################################################################
 # CONSTANTES DO PROJETO                                                                                         #
@@ -94,24 +129,59 @@ if __name__ == '__main__':
 # Etapas para resolução do projeto
 # 1 - Achar o tamanho da chave
     # 1.1 - Print out cyphertext on a long single strip twice
-    
+    from utils import *
+    mensagem_tratada = remove_combining_fluent(desafio_txt1)
+    mensagem_tratada = remove_caracteres_especiais_e_espacos(mensagem_tratada)
+    print(mensagem_tratada)
+        
     
     # 1.2 - Slide the strip across itself until the last letter of the cyphertext matches the first letter of the
     # second copy of the cyphertext
+    
+
+    
+    
     # 1.3 - Find and count caracters coincidences
+    
     # 1.4 - Count the number of letters between the two matching letters
+    
     # 1.5 - Repeat steps 1.2 and 1.3 for each letter in the cyphertext
+   
     # 1.6 - The most common distance between matching letters is the length of the key
+    
     # 1.7 - If the key is longer than 10 characters, it is unlikely that the key is longer than 10 characters
+    
     # 1.8 - If the key is shorter than 3 characters, it is unlikely that the key is shorter than 3 characters
+    
     # 1.9 - If the key is 3 characters long, it is likely that the key is "the"
+    
     # 1.10 - If the key is 2 characters long, it is likely that the key is "of", "to", "in", "it", "is", "be", "as", "at",
     # "so", "we", "he", "by", "or", "on", "do", "if", "me", "my", "up", "an", "go", "no", "us", "am"
+    
     # 1.11 - If the key is 1 character long, it is likely that the key is "a", "i"
     
-    # 
+    # 1.12 - If the key is 4 characters long, it is likely that the key is "that", "have", "with", "this", "will", "your",
+    # "from", "they", "know", "want", "been", "good", "much", "some", "time"
+    
+    # 1.13 - If the key is 5 characters long, it is likely that the key is "their", "there", "would", "could", "about",
+    # "which", "these", "other", "words", "after", "first", "thing", "those", "where", "sound", "water", "place",
+    # "right", "think", "three", "years", "world", "still", "small", "never", "under", "while", "along", "might",
+    # "house", "found", "light", "large", "every", "since", "between", "under", "being", "leave", "heart", "story",
+    # "today", "great", "paper", "often", "until", "night", "always", "money", "music", "point", "until", "night",
+    # "always", "money", "music", "point", "until", "night", "always", "money", "music", "point", "until", "night",
+    
+    # 1.14 - If the key is 6 characters long, it is likely that the key is "people", "really", "family", "school",
+    # "second", "around", "friend", "father", "mother", "before", "though"
+    
+    # 1.15 - If the key is 7 characters long, it is likely that the key is "because", "through", "thought", "without",
+    # "nothing", "another", "against", "without", "nothing", "another", "against", "without", "nothing", "another",
+    # "against", "without", "nothing", "another", "against", "without", "nothing", "another", "against", "without",
+    
+    
+    
     
     
 
 
-# 2 - Descobrir a chave em si (qual a letra que foi rotacionada)
+# 2 - Discover the key per se (witch letter was rotated)
+
---
+++ app/parte-2/remove_accents.py
@@ -0,0 +1,44 @@
+#https://www.youtube.com/watch?v=F_gYj6fcSl4
+#https://gist.github.com/luizomf/54b58615cd674db44153470c369a8284#file-remove_accents-py
+
+
+import unicodedata
+import re
+
+
+def remove_non_ascii(string: str) -> str:
+    return string.encode('ascii', 'ignore').decode('utf8').casefold()
+
+
+def remove_non_ascii_normalized(string: str) -> str:
+    normalized = unicodedata.normalize('NFD', string)
+    return normalized.encode('ascii', 'ignore').decode('utf8').casefold()
+
+
+def remove_combining_regex(string: str) -> str:
+    normalized = unicodedata.normalize('NFD', string)
+    return re.sub(r'[\u0300-\u036f]', '', normalized).casefold()
+
+
+def remove_combining_fluent(string: str) -> str:
+    normalized = unicodedata.normalize('NFD', string)
+    return ''.join(
+        [l for l in normalized if not unicodedata.combining(l)]
+    ).casefold()
+
+
+if __name__ == "__main__":
+    string = 'Atenção \N{SNAKE}'
+
+    # normalized = unicodedata.normalize('NFD', string)
+    # print([(l, f'U+{ord(l):04x}', unicodedata.name(l)) for l in normalized])
+
+    print(string)
+    print(remove_non_ascii(string))
+    print(remove_non_ascii_normalized(string))
+    print(remove_combining_regex(string))
+    print(remove_combining_fluent(string))
+
+    print(
+        remove_combining_regex('Otávio') == remove_combining_fluent('otavio')
+    )
