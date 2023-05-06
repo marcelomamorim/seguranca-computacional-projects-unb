54ce7f1f956a8a92f372a518f4bea03cfd39cd9e
Marcelo <marcelo.med.amorim@gmail.com>
2023-05-06 17:02:45 -0300

    finalizando descobrimento de senha

--- app/parte-2/main.py
+++ app/parte-2/main.py
@@ -17,6 +17,31 @@ kakv tf thv syip.  khvre zs eotyieg slrgrijieg ie tyis.  zf khep blt
 keen it, rldosk acl mvn zn tyezr dvgiee, jode tzmv or ftyer, thvrijh
 merp nvarcy khe jade fvecinxs kowrrus tye fcern nity mv.'''
 
+desafio_txt2 = '''tpsja kexis ttgztpb wq ssmil tfdxf vsetw ytafrttw btzf pcbroxdzo zn tqac wix, bwfd s, je ahvup sd 
+pcbqqxff lfzed d avu ytwoxavneh sg p aznst qaghv. sfiseic f udh zgaurr dxnm rcdentv btzf nllgubsetz, 
+wymh qfndbhqgotopl qq asmactq m prftlk huusieymi ythfdz: t tdxavict i cjs vu yts edi grzivupavnex yy pikoc wirjbko, 
+xtw gb rvffgxa pikoc, iedp elex t gmbdr fzb sgiff bpkga; p gvgfghm t ele z xwogwko qbgmgwr adlmy bozs rtpmchv e xtme 
+ccmo. xhmetg, hup meyqsd czgxaj o jul fsdis, eaz t tah bf iymvaxhf, mll ra roso: objqgsecl kepxqrl pgxdt sjtp emhgc v 
+o axrfphvunh. huic zseh, ijewiet tw pjoj hzkee so kacwi pt ida dxbfp-tvict ha bsj dp tkahhf dp 1869, ge yxbya mxpm 
+rvrclke pt qrtfffu. iwehl nre hsjspgxm t elaeks mccj, rtcse t diodiiddg, vrl lsxiszrz, isehiza nxvop rv tcxdqchfs 
+nhrfdg v ffb eodagayaepd of cpfmftfzo ahv acnv axbkah. cezp tquvcj! vpkhmss v qfx rmd vfugx gmghrs yxq mciecthw. 
+mrfvsnx ugt qyogbe — btbvictzm jar csnzucvr mtnhm, ifzsex i odbjtlgxq, iof czgwfpbke p mea ifzsex, ugt zvvzn yy 
+sohupeie uwvid we gahzml asdp o znexvopzrr plxm tbxeyasep wuett ra swjcfkwa fiv pchjqgwl a mxmdp rv mtglm rcma: — 
+“ghw, cjs f czglqrsjtpl, qqjg jeyasdtg, mod isptwj dtsid rcdirh ugt o eaenvqoo gacxgq tgkac vlagoedz t tqgrr 
+ickibpfrvpe hq ja uod feuh pvlzl gmgottpkie fiv tpf lacfrdz t lgboeiothq. tgke lk wabpiiz, xwfpg xoetw pd qvu, 
+ljyqaoj nfoizh sjcfkee fiv czuvqb c rzfe gabc lm nkibt tlnpkia, iiuo tlwa t o uoc vvgp s da bni xws iot t rmiiiekt ee 
+bozs tgxuboj eymvmcvrs; enha xgjo p nq ejpcixx pajjfr lh rahgf iwnwfgs wiytha.” qcd e qbix pazgz! gea, 
+cof mp tvdtdvnoh hmh jznex ebdzzcpl ugt zye oxmjtw. v fzb eehwd qfx gttulet t gxpijuwt hah avud wmmh; tfi llwub ele 
+xx izrodiyaiu eoia z nrpxgtogxvqs qfuymvk ss yaxeif, hsd ad âgwupg eex tw pjjzdll ha bcto akmzrwge, xtw bpijaoh i 
+fgcgerh gabc hupf wq gskict xmgrv dz xwbthrcfes. fpfue p tfagfvctws. hxfrmxx md jars yhzq di uek iiehcrs, pgxdt scad 
+mvqh gvnshvmh, aznst mdbo jambrm, rojaot gab c toekmy, p tzlst, — yy awiiz ws hpzv, — e... exrtpa ganbizrwr! dljyu p 
+dfunh pttg uicxm cjsd ect e ftftetke etbyoct. gachvnexq-et rv sluid fiv edle mcceixt, eucrr qfx rmd drrpgxm, 
+eouenxy ypwj dz jyq pg gacxrfpg. v vpkhmss, gaoxgqj arid. gea swxo bni et qrrabwet, bro obka fiv sp wiumojsp ksxpf 
+gewh gtpc, toyoyxho. eex h qqj csieh idp qfidt exiodeymi pgodaebgm... ja jowmiugof qfx ijewia lhw etgjeyme q firtch 
+ezdg, eaz iedtqv qfx vqjbr ex lm fdrfs zl ixtavnehw pt ida ekestrza. p wepd ele dbq, a fiv mpgse rcevtglm p sjsl 
+tracwda pke meoieyme-xd. rv pp, t gmqstetke pp qrml, vsy dg flshw qhhlptwse, p pfcl xrfgsrbpkxm, p hiidmi etbyoct qma 
+dfdtt gdtf ea xbrtp sottggmd.'''
+
 #################################################################################################################
 #################################################################################################################
 # CONSTANTES DO PROJETO                                                                                         #
@@ -33,32 +58,20 @@ frequencia_letras_ingles = [0.0812, 0.0149, 0.0271, 0.0432, 0.1202, 0.0230, 0.02
                             0.0398, 0.0261, 0.0695, 0.0768, 0.0182, 0.0011, 0.0602, 0.0628, 0.091, 0.0288, 0.0111,
                             0.0209, 0.0017, 0.0211, 0.0007]
 
+# https://www.gta.ufrj.br/grad/06_2/alexandre/criptoanalise.html
+frequencia_letras_portugues = [0.1463, 0.0104, 0.0388, 0.0499, 0.1256, 0.0102, 0.0130, 0.0128, 0.0618, 0.004, 0.0002,
+                               0.0278, 0.0474, 0.0505, 0.1073, 0.0252, 0.012, 0.0653, 0.0781, 0.0434, 0.0463, 0.0167,
+                               0.0001, 0.0021, 0.0001, 0.0047]
+
 # Alfabeto na mesma ordem da tabela asc ii: https://www.asciitable.com/
 alfabeto = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
             'v', 'w', 'x', 'y', 'z']
 
-# Juntando as duas listas num dicionário :::::::::::::::::::::>
-dicionario_frequencia_letras_ingles = {
-    'a': 0.0812, 'b': 0.0149, 'c': 0.0271,
-    'd': 0.0432, 'e': 0.1202, 'f': 0.0230,
-    'g': 0.0203, 'h': 0.0592, 'i': 0.0731,
-    'j': 0.0001, 'k': 0.0069, 'l': 0.0398,
-    'm': 0.0261, 'n': 0.0695, 'o': 0.0768,
-    'p': 0.0182, 'q': 0.0011, 'r': 0.0602,
-    's': 0.0628, 't': 0.091, 'u': 0.0288,
-    'v': 0.0111, 'w': 0.0209, 'x': 0.0017,
-    'y': 0.0211, 'z': 0.0007
-}
-
 
 def cria_lista_de_caracteres_partindo_do_texto_inicial(texto_criptografado):
-    not_allowed_list = ['', ' ', '  ', '-', '--', '.', ',', ';', '\n']
     texto_tratado = list(texto_criptografado)
-    for char in texto_tratado:
-        if char in not_allowed_list:
-            texto_tratado.remove(char)
-
-    return texto_tratado
+    texto_final = [caratere for caratere in texto_tratado if caratere in alfabeto]
+    return texto_final
 
 
 def agrupa_lista_em_n_grupos_de_acordo_com_o_tamanho_da_chave(tamanho_da_chave, caracteres):
@@ -104,7 +117,7 @@ def gera_lista_frequencia_caractere(grupos_caracteres):
 
 
 # (caso base) multiplicar frequencia das letras do grupo n com a frequencia da lingua
-def soma_produtos_frequencia_na_cifra_com_frequencia_na_lingua(frequencia_lingua, frequencia_txt_cifrado):
+def soma_produtos_frequencia_na_cifra_com_frequencia_na_lingua_inglesa(frequencia_lingua, frequencia_txt_cifrado):
     lista_probabilidades = []
 
     for i in range(len(frequencia_txt_cifrado)):
@@ -117,6 +130,29 @@ def soma_produtos_frequencia_na_cifra_com_frequencia_na_lingua(frequencia_lingua
             soma_dos_produtos = 0
             for t in range(len(lista_valores)):
                 soma_dos_produtos += (lista_valores[t] * frequencia_letras_ingles[t])
+
+            frequencias_i.append(soma_dos_produtos)
+            if k == len(grupo_i) - 1:
+                lista_probabilidades.append(frequencias_i)
+
+    print(lista_probabilidades)
+    return lista_probabilidades
+
+
+# (caso base) multiplicar frequencia das letras do grupo n com a frequencia da lingua
+def soma_produtos_frequencia_na_cifra_com_frequencia_na_lingua_portuguesa(frequencia_lingua, frequencia_txt_cifrado):
+    lista_probabilidades = []
+
+    for i in range(len(frequencia_txt_cifrado)):
+        # soma dos produtos das frequencias do grupo 'i'
+        frequencias_i = []
+        for k in range(len(frequencia_lingua)):
+            grupo_i = dict(rotaciona_dicionario(-k, frequencia_txt_cifrado[i]))
+            lista_valores = list(grupo_i.values())
+            print('lista valores ::>' + str(lista_valores))
+            soma_dos_produtos = 0
+            for t in range(len(lista_valores)):
+                soma_dos_produtos += (lista_valores[t] * frequencia_letras_portugues[t])
 
             frequencias_i.append(soma_dos_produtos)
             if k == len(grupo_i) - 1:
@@ -153,11 +189,11 @@ def encontra_maior_probabilidade_e_conta_quantas_vezes_foi_rotacionado(lista_pro
 
 
 if __name__ == '__main__':
-    lista_caracteres = cria_lista_de_caracteres_partindo_do_texto_inicial(desafio_txt1)
+    lista_caracteres = cria_lista_de_caracteres_partindo_do_texto_inicial(desafio_txt2)
 
     print('LISTA DE CARACTERES ::::::::::::>' + str(lista_caracteres))
 
-    grupos_vetor_bidimensional = agrupa_lista_em_n_grupos_de_acordo_com_o_tamanho_da_chave(5, lista_caracteres)
+    grupos_vetor_bidimensional = agrupa_lista_em_n_grupos_de_acordo_com_o_tamanho_da_chave(8, lista_caracteres)
 
     print('GRUPOS :::::::::::::::>' + str(grupos_vetor_bidimensional))
 
@@ -165,8 +201,15 @@ if __name__ == '__main__':
 
     print('DICIONÁRIO DE FREQUENCIA :::::::::::::>' + str(dicionario_frequencia))
 
-    lista_de_probabilidades = soma_produtos_frequencia_na_cifra_com_frequencia_na_lingua(
-        dicionario_frequencia_letras_ingles,
+    lista_de_probabilidades_ingles = soma_produtos_frequencia_na_cifra_com_frequencia_na_lingua_inglesa(
+        frequencia_letras_ingles,
         dicionario_frequencia)
 
-    encontra_maior_probabilidade_e_conta_quantas_vezes_foi_rotacionado(lista_probabilidades=lista_de_probabilidades)
+    lista_de_probabilidades_portugues = soma_produtos_frequencia_na_cifra_com_frequencia_na_lingua_portuguesa(
+        frequencia_letras_portugues,
+        dicionario_frequencia)
+
+    encontra_maior_probabilidade_e_conta_quantas_vezes_foi_rotacionado(
+        lista_probabilidades=lista_de_probabilidades_ingles)
+    encontra_maior_probabilidade_e_conta_quantas_vezes_foi_rotacionado(
+        lista_probabilidades=lista_de_probabilidades_portugues)
