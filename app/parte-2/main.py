1bb88d8f02fcee5c094ce4ceee6391d9fca0f397
Marcelo <marcelo.med.amorim@gmail.com>
2023-05-06 18:42:22 -0300

    refatoração no código

--- app/parte-2/main.py
+++ app/parte-2/main.py
@@ -3,45 +3,6 @@
 # Youtube: https://www.youtube.com/watch?v=LaWp_Kq0cKs                  #
 #########################################################################
 
-desafio_txt1 = '''rvgllakieg tye tirtucatzoe.  whvnvvei i
-winu mpsecf xronieg giid abfuk thv mfuty; wyenvvvr ik ij a drmg,
-drzzqly eomemsei in dy jouc; wyenvvvr i wied mpsvlf znmollnkarzlp
-palszng seworv cfffzn narvhfusvs, rnd srzngznx up khv rerr ff emeiy
-flnvrac i deek; aed ejpvcirlcy wyeeevvr dy hppfs gvt jucy ae upgei
-haed ff mv, tyat zt ieqliies r skroeg dorrl grieczplv tf prvvvnt de
-wrod dvliseiatvlp stvpginx ieto khv stievt, aed detyouicrlcy keotkieg
-geoglv's hrtj ofw--tyen, z atcolnk it yixh tzmv to xek to jer as jofn
-aj i tan.  khzs ij mp susskitltv foi pzstfl rnd sacl.  wzty a
-pyicosfpyicrl wlolrzsh tako tyrfws yidsecf lpoe hzs snoid; i huzetcy
-kakv tf thv syip.  khvre zs eotyieg slrgrijieg ie tyis.  zf khep blt
-keen it, rldosk acl mvn zn tyezr dvgiee, jode tzmv or ftyer, thvrijh
-merp nvarcy khe jade fvecinxs kowrrus tye fcern nity mv.'''
-
-desafio_txt2 = '''tpsja kexis ttgztpb wq ssmil tfdxf vsetw ytafrttw btzf pcbroxdzo zn tqac wix, bwfd s, je ahvup sd 
-pcbqqxff lfzed d avu ytwoxavneh sg p aznst qaghv. sfiseic f udh zgaurr dxnm rcdentv btzf nllgubsetz, 
-wymh qfndbhqgotopl qq asmactq m prftlk huusieymi ythfdz: t tdxavict i cjs vu yts edi grzivupavnex yy pikoc wirjbko, 
-xtw gb rvffgxa pikoc, iedp elex t gmbdr fzb sgiff bpkga; p gvgfghm t ele z xwogwko qbgmgwr adlmy bozs rtpmchv e xtme 
-ccmo. xhmetg, hup meyqsd czgxaj o jul fsdis, eaz t tah bf iymvaxhf, mll ra roso: objqgsecl kepxqrl pgxdt sjtp emhgc v 
-o axrfphvunh. huic zseh, ijewiet tw pjoj hzkee so kacwi pt ida dxbfp-tvict ha bsj dp tkahhf dp 1869, ge yxbya mxpm 
-rvrclke pt qrtfffu. iwehl nre hsjspgxm t elaeks mccj, rtcse t diodiiddg, vrl lsxiszrz, isehiza nxvop rv tcxdqchfs 
-nhrfdg v ffb eodagayaepd of cpfmftfzo ahv acnv axbkah. cezp tquvcj! vpkhmss v qfx rmd vfugx gmghrs yxq mciecthw. 
-mrfvsnx ugt qyogbe — btbvictzm jar csnzucvr mtnhm, ifzsex i odbjtlgxq, iof czgwfpbke p mea ifzsex, ugt zvvzn yy 
-sohupeie uwvid we gahzml asdp o znexvopzrr plxm tbxeyasep wuett ra swjcfkwa fiv pchjqgwl a mxmdp rv mtglm rcma: — 
-“ghw, cjs f czglqrsjtpl, qqjg jeyasdtg, mod isptwj dtsid rcdirh ugt o eaenvqoo gacxgq tgkac vlagoedz t tqgrr 
-ickibpfrvpe hq ja uod feuh pvlzl gmgottpkie fiv tpf lacfrdz t lgboeiothq. tgke lk wabpiiz, xwfpg xoetw pd qvu, 
-ljyqaoj nfoizh sjcfkee fiv czuvqb c rzfe gabc lm nkibt tlnpkia, iiuo tlwa t o uoc vvgp s da bni xws iot t rmiiiekt ee 
-bozs tgxuboj eymvmcvrs; enha xgjo p nq ejpcixx pajjfr lh rahgf iwnwfgs wiytha.” qcd e qbix pazgz! gea, 
-cof mp tvdtdvnoh hmh jznex ebdzzcpl ugt zye oxmjtw. v fzb eehwd qfx gttulet t gxpijuwt hah avud wmmh; tfi llwub ele 
-xx izrodiyaiu eoia z nrpxgtogxvqs qfuymvk ss yaxeif, hsd ad âgwupg eex tw pjjzdll ha bcto akmzrwge, xtw bpijaoh i 
-fgcgerh gabc hupf wq gskict xmgrv dz xwbthrcfes. fpfue p tfagfvctws. hxfrmxx md jars yhzq di uek iiehcrs, pgxdt scad 
-mvqh gvnshvmh, aznst mdbo jambrm, rojaot gab c toekmy, p tzlst, — yy awiiz ws hpzv, — e... exrtpa ganbizrwr! dljyu p 
-dfunh pttg uicxm cjsd ect e ftftetke etbyoct. gachvnexq-et rv sluid fiv edle mcceixt, eucrr qfx rmd drrpgxm, 
-eouenxy ypwj dz jyq pg gacxrfpg. v vpkhmss, gaoxgqj arid. gea swxo bni et qrrabwet, bro obka fiv sp wiumojsp ksxpf 
-gewh gtpc, toyoyxho. eex h qqj csieh idp qfidt exiodeymi pgodaebgm... ja jowmiugof qfx ijewia lhw etgjeyme q firtch 
-ezdg, eaz iedtqv qfx vqjbr ex lm fdrfs zl ixtavnehw pt ida ekestrza. p wepd ele dbq, a fiv mpgse rcevtglm p sjsl 
-tracwda pke meoieyme-xd. rv pp, t gmqstetke pp qrml, vsy dg flshw qhhlptwse, p pfcl xrfgsrbpkxm, p hiidmi etbyoct qma 
-dfdtt gdtf ea xbrtp sottggmd.'''
-
 #################################################################################################################
 #################################################################################################################
 # CONSTANTES DO PROJETO                                                                                         #
@@ -72,6 +33,20 @@ def cria_lista_de_caracteres_partindo_do_texto_inicial(texto_criptografado):
     texto_tratado = list(texto_criptografado)
     texto_final = [caratere for caratere in texto_tratado if caratere in alfabeto]
     return texto_final
+
+
+# Método não finalizado (registro do desenvolvimento)
+def encontra_tamanho_da_chave(texto_como_lista):
+    lista_coincidencias = []
+    copia_do_input = texto_como_lista.copy()
+    for i in range(len(copia_do_input)):
+        coincidencias = 0
+        copia_do_input_analisada = copia_do_input[(i + 1):]
+        for k in range(len(copia_do_input_analisada)):
+            if copia_do_input_analisada[k] == copia_do_input[k]:
+                coincidencias += 1
+            if k == len(copia_do_input_analisada) - 1:
+                lista_coincidencias.append(coincidencias)
 
 
 def agrupa_lista_em_n_grupos_de_acordo_com_o_tamanho_da_chave(tamanho_da_chave, caracteres):
@@ -178,13 +153,16 @@ if __name__ == '__main__':
 
     expected_language = input("Escolha a linguagem que imagina que esteja o texto [pt ou en] ::> ")
     texto_escolhido = input("Escolha entre o texto 1 (1) e o texto 2 (2) [1 ou 2]::> ")
+    tamanho_chave = input("Escolha o tamanho da chave :::>")
     texto_criptografado = ''
     lista_frequencias_por_lingua = []
 
     if texto_escolhido == '1':
-        texto_criptografado = desafio_txt1
+        with open('texto1.txt', 'r') as file:
+            texto_criptografado = file.read().replace('\n', '')
     else:
-        texto_criptografado = desafio_txt2
+        with open('texto2.txt', 'r') as file:
+            texto_criptografado = file.read().replace('\n', '')
 
     if expected_language == 'en':
         lista_frequencias_por_lingua = frequencia_letras_ingles
@@ -193,7 +171,8 @@ if __name__ == '__main__':
 
     lista_caracteres = cria_lista_de_caracteres_partindo_do_texto_inicial(texto_criptografado)
 
-    grupos_vetor_bidimensional = agrupa_lista_em_n_grupos_de_acordo_com_o_tamanho_da_chave(5, lista_caracteres)
+    grupos_vetor_bidimensional = agrupa_lista_em_n_grupos_de_acordo_com_o_tamanho_da_chave(int(tamanho_chave),
+                                                                                           lista_caracteres)
 
     dicionario_frequencia = gera_lista_frequencia_caractere(grupos_caracteres=grupos_vetor_bidimensional)
 
@@ -209,4 +188,3 @@ if __name__ == '__main__':
     print("PALAVRA CHAVE :::::::::::::::::> " + str(palavra_encontrada))
     print("####################################################################")
     print("####################################################################")
-
